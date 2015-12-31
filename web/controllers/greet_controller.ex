54774e8de6d1df0ee7b2f6379450d968175e0b3a
Nhu Nguyen <nhu313@gmail.com>
2015-12-31 05:49:29 -0500

    Added route params

--- web/controllers/greet_controller.ex
+++ web/controllers/greet_controller.ex
@@ -1,7 +1,11 @@
 defmodule Blog.GreetController do
   use Blog.Web, :controller
 
-  def hello(conn, _params) do
-    render conn, "hello.html"
+  def hello(conn, %{"name" => user_name}) do
+    render conn, "hello.html", user_name: user_name
+  end
+
+  def name(conn, %{"name" => name}) do
+    render conn, "hello.html", user_name: name
   end
 end
